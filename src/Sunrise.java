bc3af4fe6822932b18524e1a977f09ef4902e850
Amruthakrishnan123 <amruthakrishnan936@gmail.com>
2026-04-06 14:04:45 +0530

    exp17done

--- src/Sunrise.java
+++
@@ -1,8 +0,0 @@
-public class Sunrise {
-   
-        // TODO: Read n
-        // TODO: Read the array of heights
-        // TODO: Iterate through the array and count how many buildings are taller than the current maximum height encountered so far.
-        // TODO: Print the count
-    
-}
